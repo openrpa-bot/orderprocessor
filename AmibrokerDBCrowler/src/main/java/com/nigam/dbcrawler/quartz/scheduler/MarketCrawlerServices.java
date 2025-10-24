880dc115ff8af8202fc8142910c3598b695d47db
Anupriya <rahulnigamfzd@gmail.com>
2025-10-24 11:07:32 +0530

    fix

--- AmibrokerDBCrowler/src/main/java/com/nigam/dbcrawler/quartz/scheduler/MarketCrawlerServices.java
+++ AmibrokerDBCrowler/src/main/java/com/nigam/dbcrawler/quartz/scheduler/MarketCrawlerServices.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.TimeZone;
 
 import java.util.Objects;
 import java.util.Optional;
